83efde3d3c83ebf1b3845f3eba45cddc4eecd9f1
Musab Alzoubi <musab.m.alzoubii@gmail.com>
2025-08-25 23:35:34 +0300

    dd

--- chatbot-service/chatbot-service/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export TITLE="Rasa api"
-gnome-terminal -- bash -c "echo -ne \"\033]0;${TITLE}\007\"; source venv/bin/activate; rasa run --enable-api --cors '*' --debug; exec bash"
-
-export TITLE="action server"
-gnome-terminal -- bash -c "echo -ne \"\033]0;${TITLE}\007\"; source venv/bin/activate; rasa run actions; exec bash"
